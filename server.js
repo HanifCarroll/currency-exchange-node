fb4a8ae30c6c30d4f3243f2d74ae3379a2bbaba9
Hanif Carroll <hanif.carroll@capgemini.com>
2019-02-01 14:30:06 -0500

    Quick fix.

--- server.js
+++ server.js
@@ -6,10 +6,14 @@ const app = express();
 app.use(cors());
 
 app.get('/exchange/:from/:to/:amount', (req, res) => {
-  const { from, to, amount } = req.params;
-  const total = convert(from, to, Number(amount)).toString();
+  let { from, to, amount } = req.params;
+  let total;
+  amount = Number(amount);
 
-  res.send(total);
+  if (amount > 0) {
+    total = convert(from, to, amount).toString();
+    res.send(total);
+  }
 });
 
 app.listen(8080, () => console.log('Server started...'));
