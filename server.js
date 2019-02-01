f1da3df2da870594c0c4d9d770473bf13f089e70
Hanif Carroll <hanif.carroll@capgemini.com>
2019-02-01 11:41:08 -0500

    Fix conflicts.

--- server.js
+++ server.js
@@ -5,11 +5,6 @@ const convert = require('./convert');
 const app = express();
 app.use(cors());
 
-<<<<<<< HEAD
-app.use(cors());
-
-=======
->>>>>>> fdee43294901525d2ea49a7cf934254e4d7e0e0b
 app.get('/exchange/:from/:to/:amount', (req, res) => {
   const { from, to, amount } = req.params;
   const total = convert(from, to, Number(amount)).toString();
